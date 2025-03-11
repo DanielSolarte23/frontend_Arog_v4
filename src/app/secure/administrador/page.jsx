1428cb200f854a531df3b390842e7bd8525fcd8a
ricardo andres erazo <ricardoandreserazomunoz@gmail.com>
2025-03-11 14:17:03 -0500

    cambios ricardo

--- src/app/secure/administrador/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function pageAdministrador() {
-  return (
-    <div>pageAdministrador</div>
-  )
-}
-
-export default pageAdministrador
---
+++ src/app/secure/prueba/page.jsx
@@ -0,0 +1,13 @@
+import Pago from '../pagos/pago'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <Pago/>
+
+    </div>
+  )
+}
+
+export default page
