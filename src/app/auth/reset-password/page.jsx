64b29244d449867c09de52ad1099729c61d6a7cf
DanielSolarte23 <royersolarte22@gmail.com>
2025-04-09 23:06:35 -0500

    token reset password 2

--- src/app/auth/reset-password/page.jsx
+++ src/app/auth/reset-password/page.jsx
@@ -1,19 +1,33 @@
+// app/auth/reset-password/page.js
 'use client';
 import { useSearchParams } from 'next/navigation';
-import ResetPasswordForm from '@/components/publicas/ResetPassword'
-import React from 'react';
+import ResetPasswordForm from '@/components/ResetPasswordForm'; // Ajusta la ruta
+import React, { Suspense } from 'react';
+import LoadingScreenIni from '@/components/publicas/LoadingScreenInicio';
 
 export const dynamic = 'force-dynamic';
 
-function ResetPasswordPage() {
+// Componente de carga mientras se resuelve la suspensión
+function Loading() {
+  return <LoadingScreenIni />;
+}
+
+// Componente que usa useSearchParams dentro de Suspense
+function ResetPasswordContent() {
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
 
+  return <ResetPasswordForm initialToken={token} />;
+}
+
+function ResetPasswordPage() {
   return (
     <div className='hoja-dos h-full'>
-      <ResetPasswordForm initialToken={token} />
+      <Suspense fallback={<Loading />}>
+        <ResetPasswordContent />
+      </Suspense>
     </div>
-  )
+  );
 }
 
 export default ResetPasswordPage;
